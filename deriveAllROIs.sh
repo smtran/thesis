5d2f205887b981a35b4385b8c9af52d276b4c61b
smtran <stellatran@live.com>
2014-07-17 10:46:03 -0400

    included posterior motor-attention regions

--- deriveAllROIs.sh
+++ deriveAllROIs.sh
@@ -32,52 +32,52 @@ outdirAtlasExtraction=${repoParent}/thesis/regions/AtlasExtraction
 
 #left precuneus
 fslmaths \
-$FSLDIR/data/atlases/HarvardOxford/HarvardOxford-cortl-maxprob-thr25-1mm.nii.gz \
+${outdirAtlasExtraction}/cortl_slicedPosteriorAttentionROIs.nii.gz \
 -thr 61 -uthr 61 -bin \
 ${outdirAtlasExtraction}/LH-Precu-mask-bin.nii.gz \
 -odt char 
 
 fslmaths \
-$FSLDIR/data/atlases/HarvardOxford/HarvardOxford-cortl-maxprob-thr25-1mm.nii.gz \
+${outdirAtlasExtraction}/cortl_slicedPosteriorAttentionROIs.nii.gz \
 -thr 61 -uthr 61 \
 ${outdirAtlasExtraction}/LH-Precu-mask-orig.nii.gz \
 -odt char 
 
 #left cingulate gyrus
 fslmaths \
-$FSLDIR/data/atlases/HarvardOxford/HarvardOxford-cortl-maxprob-thr25-1mm.nii.gz \
+${outdirAtlasExtraction}/cortl_slicedPosteriorAttentionROIs.nii.gz \
 -thr 59 -uthr 59 -bin \
 ${outdirAtlasExtraction}/LH-CingulatePosterior-mask-bin.nii.gz \
 -odt char 
 
 fslmaths \
-$FSLDIR/data/atlases/HarvardOxford/HarvardOxford-cortl-maxprob-thr25-1mm.nii.gz \
+${outdirAtlasExtraction}/cortl_slicedPosteriorAttentionROIs.nii.gz \
 -thr 59 -uthr 59 \
 ${outdirAtlasExtraction}/LH-CingulatePosterior-mask-orig.nii.gz \
 -odt char 
 
 #right precuneus
 fslmaths \
-$FSLDIR/data/atlases/HarvardOxford/HarvardOxford-cortl-maxprob-thr25-1mm.nii.gz \
+${outdirAtlasExtraction}/cortl_slicedPosteriorAttentionROIs.nii.gz \
 -thr 62 -uthr 62 -bin \
 ${outdirAtlasExtraction}/RH-Precu-mask-bin.nii.gz \
 -odt char 
 
 fslmaths \
-$FSLDIR/data/atlases/HarvardOxford/HarvardOxford-cortl-maxprob-thr25-1mm.nii.gz \
+${outdirAtlasExtraction}/cortl_slicedPosteriorAttentionROIs.nii.gz \
 -thr 62 -uthr 62 \
 ${outdirAtlasExtraction}/RH-Precu-mask-orig.nii.gz \
 -odt char 
 
 #right cingulate gyrus
 fslmaths \
-$FSLDIR/data/atlases/HarvardOxford/HarvardOxford-cortl-maxprob-thr25-1mm.nii.gz \
+${outdirAtlasExtraction}/cortl_slicedPosteriorAttentionROIs.nii.gz \
 -thr 60 -uthr 60 -bin \
 ${outdirAtlasExtraction}/RH-CingulatePosterior-mask-bin.nii.gz \
 -odt char
 
 fslmaths \
-$FSLDIR/data/atlases/HarvardOxford/HarvardOxford-cortl-maxprob-thr25-1mm.nii.gz \
+${outdirAtlasExtraction}/cortl_slicedPosteriorAttentionROIs.nii.gz \
 -thr 60 -uthr 60 \
 ${outdirAtlasExtraction}/RH-CingulatePosterior-mask-orig.nii.gz \
 -odt char
@@ -483,5 +483,72 @@ fslmaths ${outdirAtlasExtraction}/LH-preSMAjuxtaLobule-mask-orig.nii.gz \
  ${outdirTempROI}/preSupplementaryMotorArea.nii.gz
 
 
-###################### Posterior MOTOR Control: PosteriorCingulate/PreCun #########
-#
+###################### Posterior MOTOR Attention: PosteriorCingulate/PreCun ##########
+#Regions from LATERALIZED (cortl) H0 Cortical Atlas 
+#61 L Precuneus cortex
+#59 L Cingulate gyrus, posterior division
+#62 R Precuneus cortex
+#60 R Cingulate gyrus, posterior division
+
+#creating one file per exisiting region
+
+#left precuneus
+fslmaths \
+${outdirAtlasExtraction}/cortl_slicedPosteriorMotorAttentionROIs.nii.gz \
+-thr 61 -uthr 61 -bin \
+${outdirAtlasExtraction}/LH-Precu-MOTOR-mask-bin.nii.gz \
+-odt char 
+
+fslmaths \
+${outdirAtlasExtraction}/cortl_slicedPosteriorMotorAttentionROIs.nii.gz \
+-thr 61 -uthr 61 \
+${outdirAtlasExtraction}/LH-Precu-MOTOR-mask-orig.nii.gz \
+-odt char 
+
+#left cingulate gyrus
+fslmaths \
+${outdirAtlasExtraction}/cortl_slicedPosteriorMotorAttentionROIs.nii.gz \
+-thr 59 -uthr 59 -bin \
+${outdirAtlasExtraction}/LH-CingulatePosterior-MOTOR-mask-bin.nii.gz \
+-odt char 
+
+fslmaths \
+${outdirAtlasExtraction}/cortl_slicedPosteriorMotorAttentionROIs.nii.gz \
+-thr 59 -uthr 59 \
+${outdirAtlasExtraction}/LH-CingulatePosterior-MOTOR-mask-orig.nii.gz \
+-odt char 
+
+#right precuneus
+fslmaths \
+${outdirAtlasExtraction}/cortl_slicedPosteriorMotorAttentionROIs.nii.gz \
+-thr 62 -uthr 62 -bin \
+${outdirAtlasExtraction}/RH-Precu-MOTOR-mask-bin.nii.gz \
+-odt char 
+
+fslmaths \
+${outdirAtlasExtraction}/cortl_slicedPosteriorMotorAttentionROIs.nii.gz \
+-thr 62 -uthr 62 \
+${outdirAtlasExtraction}/RH-Precu-MOTOR-mask-orig.nii.gz \
+-odt char 
+
+#right cingulate gyrus
+fslmaths \
+${outdirAtlasExtraction}/cortl_slicedPosteriorMotorAttentionROIs.nii.gz \
+-thr 60 -uthr 60 -bin \
+${outdirAtlasExtraction}/RH-CingulatePosterior-MOTOR-mask-bin.nii.gz \
+-odt char
+
+fslmaths \
+${outdirAtlasExtraction}/cortl_slicedPosteriorMotorAttentionROIs.nii.gz \
+-thr 60 -uthr 60 \
+${outdirAtlasExtraction}/RH-CingulatePosterior-MOTOR-mask-orig.nii.gz \
+-odt char
+
+#create subset for visualization in fslview and decisions about boundaries
+fslmaths ${outdirAtlasExtraction}/LH-Precu-MOTOR-mask-orig.nii.gz \
+-add ${outdirAtlasExtraction}/LH-CingulatePosterior-MOTOR-mask-orig.nii.gz \
+-add ${outdirAtlasExtraction}/RH-Precu-MOTOR-mask-orig.nii.gz \
+-add ${outdirAtlasExtraction}/RH-CingulatePosterior-MOTOR-mask-orig.nii.gz \
+ ${outdirTempROI}/posteriorMotorAttentionROIs.nii.gz
+
+
