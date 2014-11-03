992f832ae3f213e2a3988279cf1a1f0e9b5fc57b
Stella Tran <stellatran@live.com>
2014-11-03 15:08:49 -0500

    runallinserial

--- afniProcScripts/runAllProcScriptsInSerial20141027.sh
+++ afniProcScripts/runAllProcScriptsInSerial20141027.sh
@@ -1,5 +1,11 @@
+#!/bin/sh
 #script to execute 300 line proc script in serial 10/27/2014
 
+cd ${HOME}/temp/batch-tranThesis-20141027171752/
+
 for participant in 001 002 007 010 012 014 020 021 022 023 025 027 028 029 031 033 035 036 038 039 041 044 046 049 052 053 055 056 058 062 065 066 067 070 074 076 079; do
-	tcsh -xef proc.MA${participant}.*.onsetsBlock |& tee output.proc.MA${participant}
+	for task in language motor; do
+
+		tcsh -xef proc.MA${participant}.${task}.onsetsBlock |& tee output.proc.MA${participant}.${task}.onsetsBlock
+	done
 done
